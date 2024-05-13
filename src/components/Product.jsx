d23c81ecb897252917ddd963bbeb905aa8223c0a
Flazarte-16 <142759355+Flazarte-16@users.noreply.github.com>
2024-05-13 19:37:58 -0300

    se elimino la searchbar y se agrego una navbar con la opcion de filter y un footer

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,52 +4,58 @@ import './Product.css';
 
 const Cocktails = () => {
   const [cocktails, setCocktails] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+  const [filter, setFilter] = useState('Popular');
 
   useEffect(() => {
-    const fetchData = async (searchTerm) => {
+    const fetchData = async () => {
       try {
-        let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-        if (searchTerm) {
-          url += searchTerm;
+        let url = '';
+        if (filter === 'Popular') {
+          url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+        } else if (filter !== 'All') {
+          url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${filter}`;
         } else {
-          url += ' ';
+          url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
         }
         const response = await fetch(url);
         const data = await response.json();
         let drinks = data.drinks || [];
-        if (!searchTerm) {
-          drinks = drinks.slice(0, 6);
-        }
-        return drinks;
+        setCocktails(drinks.filter(cocktail => cocktail !== null));
       } catch (error) {
         console.error('Error fetching data:', error);
-        return null;
       }
     };
 
-    const fetchMultipleCocktails = async (searchTerm) => {
-      const cocktailData = await fetchData(searchTerm);
-      setCocktails(cocktailData ? cocktailData.filter(cocktail => cocktail !== null) : []);
-    };
+    fetchData();
+  }, [filter]); 
 
-    fetchMultipleCocktails(searchTerm);
-  }, [searchTerm]);
+  const handleFilterClick = (filter) => {
+    setFilter(filter);
+    setShowFilterOptions(false);
+  };
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const toggleFilterOptions = () => {
+    setShowFilterOptions(!showFilterOptions);
   };
 
   return (
-    <div className="cocktail-container">
-      <input 
-        type="text" 
-        placeholder="Search Drinks..." 
-        value={searchTerm} 
-        onChange={handleSearchChange} 
-      />
+    <section className="cocktail-container">
+      <nav className='navbar'>
+        <h2 className='logo'>Liquor Luxe</h2>
+        <div className="filter">
+          <div className="filter-dropdown" onClick={toggleFilterOptions}>
+            <h2 className='logo'>Filter</h2>
+            <i className={`fas fa-caret-${showFilterOptions ? 'up' : 'down'}`}></i>
+          </div>
+          <div className={`filter-options ${showFilterOptions ? 'show' : 'hide'}`}>
+            <button onClick={() => handleFilterClick('Popular')}>Popular</button>
+            <button onClick={() => handleFilterClick('Alcoholic')}>Alcoholic</button>
+            <button onClick={() => handleFilterClick('Non_Alcoholic')}>Non-Alcoholic</button>
+          </div>
+        </div>
+      </nav>
       <div>
-        <h2 className="title">Featured Products</h2>
         <div className="cocktail-list">
           {cocktails.map(cocktail => (
             <div key={cocktail.idDrink} className="cocktail-item">
@@ -62,7 +68,29 @@ const Cocktails = () => {
           ))}
         </div>
       </div>
-    </div>
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-section">
+            <h3>Redes Sociales</h3>
+            <ul className="social-links">
+              <li><a href="https://facebook.com"><i className="fab fa-facebook"></i> Facebook</a></li>
+              <li><a href="https://twitter.com"><i className="fab fa-twitter"></i> Twitter</a></li>
+              <li><a href="https://instagram.com"><i className="fab fa-instagram"></i> Instagram</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>Contacto</h3>
+            <ul className="contact-links">
+              <li><i className="fas fa-envelope"></i> contact @liquorluxe.com</li>
+              <li><i className="fas fa-phone"></i> +1 (123) 456-7890</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; 2024 Liquor Luxe. All rights reserved.</p>
+        </div>
+      </footer>
+    </section>
   );
 };
 
